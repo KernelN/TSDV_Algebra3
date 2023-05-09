304b338d665e94e8681ef93a607a13bee3bdc3fc
Alejandro Insausti <alejandroninsausti@gmail.com>
2023-05-09 18:27:00 -0300

    Add Vec3 inspector tester

---
+++ Assets/Scripts/MathDebbuger/Tests/Editor/Vec3TesterEditor.cs
@@ -0,0 +1,29 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace CustomMath
+{
+    [CustomEditor(typeof(Vec3Tester))]
+    public class Vec3TesterEditor : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            
+            Vec3Tester myScript = (Vec3Tester)target;
+            
+            //Title
+            EditorGUILayout.Separator();
+            EditorGUILayout.LabelField("Results");
+            
+            //Vector of 3 components
+            EditorGUILayout.LabelField("Vec3: ", myScript.Vec3CalcV3().ToString());
+            EditorGUILayout.LabelField("Vector3: ", myScript.Vector3CalcV3().ToString());
+            
+            //Float
+            EditorGUILayout.LabelField("Vec3: ", myScript.Vec3CalcFloat().ToString());
+            EditorGUILayout.LabelField("Vector3: ", myScript.Vector3CalcFloat().ToString());
+        }
+    }
+}
---
+++ Assets/Scripts/MathDebbuger/Tests/Vec3Tester.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+
+namespace CustomMath
+{
+    public class Vec3Tester : MonoBehaviour
+    {
+        [System.Serializable]
+        public struct V3
+        {
+            public float x;
+            public float y;
+            public float z;
+
+            #region constructors
+
+            public V3(float x, float y, float z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+
+            public V3(V3 v3)
+            {
+                this.x = v3.x;
+                this.y = v3.y;
+                this.z = v3.z;
+            }
+
+            #endregion
+
+            #region equal operators
+
+            //Vector3 myVec = new V3(0,0,0)
+            public static implicit operator Vector3(V3 v3)
+            {
+                return new Vector3(v3.x, v3.y, v3.z);
+            }
+
+            //V3 myVec = new Vector3(0,0,0)
+            public static implicit operator V3(Vector3 v3)
+            {
+                return new V3(v3.x, v3.y, v3.z);
+            }
+
+            //Vector3 myVec = new V3(0,0,0)
+            public static implicit operator Vec3(V3 v3)
+            {
+                return new Vec3(v3.x, v3.y, v3.z);
+            }
+
+            //V3 myVec = new Vector3(0,0,0)
+            public static implicit operator V3(Vec3 v3)
+            {
+                return new V3(v3.x, v3.y, v3.z);
+            }
+
+            #endregion
+        }
+
+        [SerializeField] V3[] inputs = new V3[1];
+        [SerializeField] float optionalFloat;
+
+        public Vector3 Vec3CalcV3()
+        {
+            return Vec3.Cross(inputs[0], inputs[1]);
+        }
+        public Vector3 Vector3CalcV3()
+        {
+            return Vector3.Cross(inputs[0], inputs[1]);
+        }
+        public float Vec3CalcFloat()
+        {
+            return Vec3.Dot(inputs[0], inputs[1]);
+        }
+        public float Vector3CalcFloat()
+        {
+            return Vector3.Dot(inputs[0], inputs[1]);
+        }
+    }
+}
